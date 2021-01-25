b9e9eb8f1433e9b74193af0e4e7d892255628158
root <root@ip-10-0-0-130.ap-northeast-1.compute.internal>
2021-01-25 17:09:18 +0900

    new LP release 20210125

--- user/resources/views/teaser/index.blade.php
+++ user/resources/views/teaser/index.blade.php
@@ -9,333 +9,260 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-W93HCVB');</script>
 <!-- End Google Tag Manager -->
 <meta charset="UTF-8">
+<meta name="robots" content="noindex, nofollow">
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width,user-scalable=no,shrink-to-fit=yes" />
-<meta name="author" content="人事プロパートナーズ">
-<meta name="keywords" content="人事,仕事,フリーランス,起業家,案件,週2回">
-<!-- favicon -->
-<link rel="shortcut icon" href="https://jinjiclub.com/img/favicon.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=yes">
 <title>人事倶楽部</title>
-<meta property="og:title" content="人事倶楽部">
-<meta property="og:description" content="人事について学べるオンラインサロンです。人事関連の交流を深めたり、人事についての情報を収集で
-きる場を提供します。">
-<meta property="og:url" content="https://jinjiclub.com/lp">
-<meta property="og:image" content="https://jinjiclub.com/img/ogp.png">
-<meta property="og:site_name" content="人事倶楽部">
-<meta property="og:email" content="hrpro-info@hajimari.inc">
-<meta property="og:type" content="website">
+<link rel="stylesheet" href="/teaser/css/styles.css">
+	
+<script>
+/**
+ * 到達したら要素を表示させる
+ */
+function showElementAnimation() {
 
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:site" content="@itproman_me">
-<meta name="twitter:creator" content="@itproman_me">
-<meta name="twitter:url" content="https://jinjiclub.com/lp">
-<meta name="twitter:title" content="人事倶楽部">
-<meta name="twitter:description" content="人事について学べるオンラインサロンです。人事関連の交流を深めたり、人事についての情報を収集>できる場を提供します。">
-<meta name="twitter:image" content="https://jinjiclub.com/img/ogp.png">
-<script src="https://gacraft.jp/publish/cv_10327.js"></script>
+  var element = document.getElementsByClassName('js-animation');
+  if(!element) return; // 要素がなかったら処理をキャンセル
+  
+  var showTiming = window.innerHeight > 768 ? 200 : 40; // 要素が出てくるタイミングはここで調整
+  var scrollY = window.pageYOffset;
+  var windowH = window.innerHeight;
 
-<link rel="stylesheet" href="{{ asset('teaser/lp/assets/css/reset.css') }}" />
-<link rel="stylesheet" href="{{ asset('teaser/lp/assets/css/style.css') }}" />
-<link rel="stylesheet" href="{{ asset('teaser/lp/assets/css/sp.css') }}" media="screen and (max-width: 800px)">
-<!--[if lt IE 9]>
-<script src="{{ asset('teaser/lp/assets/js/html5shiv.js') }}"></script>
-<![endif]-->
+  for(var i=0;i<element.length;i++) { var elemClientRect = element[i].getBoundingClientRect(); var elemY = scrollY + elemClientRect.top; if(scrollY + windowH - showTiming > elemY) {
+      element[i].classList.add('is-show');
+    } else if(scrollY + windowH < elemY) {
+      // 上にスクロールして再度非表示にする場合はこちらを記述
+      element[i].classList.remove('is-show');
+    }
+  }
+}
+showElementAnimation();
+window.addEventListener('scroll', showElementAnimation);
+</script>
+
+<noscript>
+  <style>
+  .js-animation {
+    opacity: 1;
+    visibility: visible;
+    transform: translateY(0px);
+  }
+  </style>
+</noscript>
+
 </head>
+	
+	
 <body>
+
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W93HCVB"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-<div id="wrap">
-	<header id="header">
-		<div class="inner">
-  		<h1><img src="{{ asset('teaser/lp/assets/img/logo.png') }}" alt=""></h1>
-		</div><!-- .inner -->
-		<div class="mv">
-  		<div class="inner">
-    		<p>
-    			<span>人事の学びとつながりを</span>
-    			<strong><em>人事</em>倶楽部</strong>
-    			～人事のオンラインサロン～
-  			</p>
-  			<div class="contact_btn">
-    			<a href="{{ route('teaser.form1') }}"><strong>入会</strong>は<span>こちら</span></a>
-  			</div><!-- .contact_btn -->
-  		</div><!-- .inner -->
-		</div><!-- .mv -->
-	</header>
-	<div id="content">
-		<section class="about">
-  		<div class="inner">
-    		<h2>人事倶楽部とは・・？</h2>
-    		<p>
-      		<span class="inner_span">人事について学べるオンラインサロンです。</span><br>
-					人事関連の交流を深めたり、人事についての情報を収集できる場を提供します。
-    		</p>
-    		<dl>
-      		<dt>▶︎▶︎ 人事倶楽部の目的 ◀︎◀︎</dt>
-      		<dd>・人事が学べる手助けをしたい</dd>
-      		<dd>・日本の人事を強くして日本の経済を発展させたい</dd>
-      		<dd>・人事や人材業界の最前線で走っている人の話を聞いてもらいたい</dd>
-      		<dd>・人事についてのノウハウを提供したい</dd>
-    		</dl>
-  		</div><!-- .inner -->
-		</section><!-- .about -->
-		<section class="works">
-  		<div class="inner">
-    		<h2>人事倶楽部でできること</h2>
-    		<ul>
-      		<li>
-      		  <dl>
-        		  <dt>動画視聴</dt>
-        		  <dd>月10~20回配信</dd>
-      		  </dl>
-      		  <p>
-        		  〈テーマ例〉<br>
-                  ・労務トラブルの対策について<br>
-        		  ・人に依存しない採用広報戦略の在り方<br>
-        		  ・人事部の立ち上げ、育成方法
-      		  </p>
-      		</li>
-      		<li>
-      		  <dl>
-        		  <dt>コミュニティへの参加</dt>
-        		  <dd>領域ごとのコミュニティ形成</dd>
-      		  </dl>
-      		  <p>
-        		  〈例〉<br>
-        		  ・採用 / 労務 / 広報 / 組織活性 / 育成 / 制度<br>
-        		  ・メンバー / マネージャー / 部門長
-      		  </p>
-      		</li>
-      		<li>
-      		  <dl>
-        		  <dt>イベントの告知・集客</dt>
-        		  <dd>随時</dd>
-      		  </dl>
-      		  <p>
-        		  〈テーマ例〉<br>
-        		  ・人事関連のイベントの告知が可能
-      		  </p>
-      		</li>
-      		<li>
-      		  <dl>
-        		  <dt>セミナーへの参加</dt>
-        		  <dd>随時実施予定</dd>
-      		  </dl>
-      		  <p>
-        		  〈テーマ例〉<br>
-                  ・人事のキャリアについて<br>
-        		  ・コロナで変わる人事評価の仕方<br>
-        		  ・人事管理領域のシステム・効率化
-      		  </p>
-      		</li>
-      		<li>
-      		  <dl>
-        		  <dt>人事相談箱の利用</dt>
-        		  <dd>人事に関する質問ができる掲示板</dd>
-      		  </dl>
-      		  <p>
-        		  〈例〉<br>
-                  ・労務について<br>
-        		  ・採用・制度設計のtips<br>
-        		  ・人事キャリアの歩み方
-      		  </p>
-      		</li>
-      		<li>
-      		  <figure>
-      		    <img src="{{ asset('teaser/lp/assets/img/works.png') }}" alt="">
-      		  </figure>
-      		</li>
-    		</ul>
-  		</div><!-- .inner -->
-		</section><!-- .works -->
-		<section class="people">
-  		<div class="inner">
-    		<h2>運営者情報</h2>
-    		<ul>
-      		<li>
-      		  <figure>
-      		    <img src="{{ asset('teaser/lp/assets/img/people01.png') }}" alt="">
-      		  </figure>
-      		  <dl>
-        		  <dt>
-        		    株式会社Hajimari<br>
-        		    執行役員 山中 諭
-        		  </dt>
-        		  <dd>
-        		    新卒でケンタッキー・フライド・チキンに入社。<br>
-                    採用／労務／制度設計／労働組合対応など、人事業務に従事した後、ウィルゲートに入社、人事部を立ち上げ、人事をやりながら広報／経理財務／営業／マーケ／エンジニア部門と幅広くマネジメントを行う。<br>
-        		    10社以上の人事コンサルに従事し、それと同時に2020年株式会社Hajimariの役員就任。
-        		  </dd>
-      		  </dl>
-      		</li>
-      		<li>
-      		  <figure>
-      		    <img src="{{ asset('teaser/lp/assets/img/people02.png') }}" alt="">
-      		  </figure>
-      		  <dl>
-        		  <dt>
-        		    株式会社GA technologies<br>
-        		    CHRO 清家 良太
-        		  </dt>
-        		  <dd>
-                    新卒で旭硝子（現AGC）に人事として入社。国内・海外人事を経験した後、アクセンチュアで人事・組織コンサル、DeNA、ビズリーチで人事に従事。ビズリーチでは事3業推進室にて事業開発も経験。2019年3月、GA technologiesにCHROとして入社。GA technologiesグループ全体の人事戦略を担う。
-        		  </dd>
-      		  </dl>
-      		</li>
-      		<li>
-      		  <figure>
-      		    <img src="{{ asset('teaser/lp/assets/img/people03.png') }}" alt="">
-      		  </figure>
-      		  <dl>
-        		  <dt>
-        		    ＨＲプラス社会保険労務士法人 <br>
-        		    代表社員 佐藤 広一
-        		  </dt>
-        		  <dd>
-        		    「ＨＲに安心、情報、ソリューションをプラスする」をコンセプトに、人事パーソンにコミットした人事労務相談、就業規則見直し、IPO支援、海外労務などの人事労務コンサルティングを展開。複数社で取締役、監査役を兼任。 著書30冊を出版、マスコミ・メディア取材、新聞・雑誌への寄稿も積極的に行う傍ら、SMBCコンサルティングなど全国各地で多数の講演を行っている。
-        		  </dd>
-      		  </dl>
-      		</li>
-      		<li>
-      		  <figure>
-      		    <img src="{{ asset('teaser/lp/assets/img/people04.png') }}" alt="">
-      		  </figure>
-      		  <dl>
-        		  <dt>
-        		    株式会社うるる<br>
-        		    取締役 小林 伸輔
-        		  </dt>
-        		  <dd>
-                    新卒で人材系上場会社に入社。トップセールスを獲得した後、2007年に株式会社うるるに参画。当社の中核事業である「入札情報速報サービス NJSS」の立ち上げや事業拡大を担った後、2017年の東証マザーズ上場後は採用・広報管掌を担い組織作りやブランド作りをミッションとしている。また、うるる社以外での活動として、中高生向けのキャリア支援教育など、若手育成にも力を入れている。
-        		  </dd>
-      		  </dl>
-      		</li>
-    		</ul>
-  		</div><!-- .inner -->
-		</section><!-- .people -->
-		<section class="support">
-  		<div class="inner">
-    		<h2>サポーター情報</h2>
-    		<ul>
-      		<li>
-      		  <figure>
-      		    <img src="{{ asset('teaser/lp/assets/img/support01.png') }}" alt="">
-      		  </figure>
-      		  <p>
-        		  株式会社ミライフ<br>
-        		  代表取締役
-        		  <strong>佐藤 雄佑</strong>
-      		  </p>
-      		</li>
-      		<li>
-      		  <figure>
-      		    <img src="{{ asset('teaser/lp/assets/img/support02.png') }}" alt="">
-      		  </figure>
-      		  <p>
-        		  アクシス株式会社<br>
-        		  代表取締役
-        		  <strong>末永 雄大</strong>
-      		  </p>
-      		</li>
-      		<li>
-      		  <figure>
-      		    <img src="{{ asset('teaser/lp/assets/img/support03.png') }}" alt="">
-      		  </figure>
-      		  <p>
-        		  株式会社パンフォーユー<br>
-        		  取締役
-        		  <strong>山口 翔</strong>
-      		  </p>
-      		</li>
-      		<li>
-      		  <figure>
-      		    <img src="{{ asset('teaser/lp/assets/img/support04.png') }}" alt="">
-      		  </figure>
-      		  <p>
-        		  株式会社トラストリッジ<br>
-        		  コーポレートデザイン部
-        		  <strong><span>部長</span>  渡部 兼蔵</strong>
-      		  </p>
-      		</li>
-    		</ul>
-  		</div><!-- .inner -->
-		</section><!-- .support -->
-		<section class="flow">
-  		<div class="inner">
-    		<h2>入会の流れ</h2>
-    		<ol>
-      		<li>
-      		  <dl>
-        		  <dt>
-        		    STEP1
-        		  </dt>
-        		  <dd>
-        		    <figure>
-        		      <img src="{{ asset('teaser/lp/assets/img/flow01.png') }}" alt="">
-        		    </figure>
-        		    <p>
-          		    会員登録
-        		    </p>
-        		  </dd>
-      		  </dl>
-      		</li>
-      		<li>
-      		  <dl>
-        		  <dt>
-        		    STEP2
-        		  </dt>
-        		  <dd>
-        		    <figure>
-        		      <img src="{{ asset('teaser/lp/assets/img/flow02.png') }}" alt="">
-        		    </figure>
-        		    <p>
-          		    運営者から<br>
-          		    コミュニティ招待
-        		    </p>
-        		  </dd>
-      		  </dl>
-      		</li>
-      		<li>
-      		  <dl>
-        		  <dt>
-        		    STEP3
-        		  </dt>
-        		  <dd>
-        		    <figure>
-        		      <img src="{{ asset('teaser/lp/assets/img/flow03.png') }}" alt="">
-        		    </figure>
-        		    <p>
-          		    入会完了
-        		    </p>
-        		  </dd>
-      		  </dl>
-      		</li>
-    		</ol>
-  		</div><!-- .inner -->
-		</section><!-- .flow -->
-		<div class="cv">
-  		<div class="cv_inner">
-    		<div class="inner">
-    		  <p><strong>入会</strong>は<span>こちら</span>から</p>
-    		  <div class="contact_btn">
-      			<a href="{{ route('teaser.form1') }}"><strong>入会</strong>は<span>こちら</span></a>
-    			</div><!-- .contact_btn -->
-    		</div><!-- .inner -->
-  		</div><!-- .cv_inner -->
-		</div><!-- .cv -->
-	</div><!-- #content -->
-	<footer id="footer">
-		<div class="inner">
 
-		</div><!-- .inner -->
-	</footer>
-</div><!-- #wrap -->
+	<div id="contents">
 
-<!-- jQuery -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="/teaser/lp/assets/js/libs/jquery-1.7.min.js">\x3C/script>')</script>
+		<!-- #header -->
+		<header id="header">
+			<div class="inner">
+				<p class="copy">人事の学びとつながりを</p>
+				<h1><img src="/teaser/img/header_logo.png" alt="人事倶楽部 JINJICLUB" width="100%"></h1>
+				<p class="subtxt">人事のオンラインサロン</p>
+				<a href="https://jinjiclub.com/form1">入会はこちら</a>
+			</div>
+		</header>
+		<!-- #header -->  
+
+
+		<!-- #about -->
+		<section id="about">
+			<div class="inner">
+				<h2 class="element js-animation">人事倶楽部とは？<br><span>ABOUT</span></h2>
+				<div class="element js-animation">
+					<p>人事の”学び”と”交流”の提供を目的とした<br>オンラインサロンです。</p>
+					<sup>※もちろん、人事以外の方の入会も大歓迎です</sup>
+				</div>
+			</div>
+		</section>
+		<!-- #about -->
+
+
+		<!-- #objective -->
+		<section id="objective">
+			<h2 class="element js-animation">人事倶楽部の目的<br><span>OBJECTIVE</span></h2>
+			<div class="inner">
+				<ul>
+					<li class="element js-animation"><img src="/teaser/img/objectiv_icon01.png" alt="objectiv_icon01" width="100%"><p>人事が学べる<br>手助けをしたい</p></li>
+					<li class="element js-animation"><img src="/teaser/img/objectiv_icon02.png" alt="objectiv_icon02" width="100%"><p>日本の人事を強くして<br>日本の経済を発展させたい</p></li>
+					<li class="element js-animation"><img src="/teaser/img/objectiv_icon03.png" alt="objectiv_icon03" width="100%"><p>人事や人材業界の<br>最前線で走っている人の話を<br>聞いてもらいたい</p></li>
+					<li class="element js-animation"><img src="/teaser/img/objective_icon04.png" alt="objectiv_icon04" width="100%"><p>人事についての<br>ノウハウを提供したい</p></li>
+				</ul>
+			</div>
+		</section>
+		<!-- #objective -->	  
+
+
+		<!-- #possible -->
+		<section id="possible">
+			<h2 class="element js-animation">人事倶楽部でできること<br><span>POSSIBLE</span></h2>
+			<!-- .inner -->
+			<div class="inner">
+				<!-- box -->
+				<div class="element js-animation">
+					<h3>動画視聴</h3>
+					<h4>月10〜20回配信</h4>
+					<dl>
+						<dt>テーマ例</dt>
+						<dd>・労務トラブルの対策について<br>・人に依存しない採用広報戦略の在り方<br>・人事部の立ち上げ、育成方法</dd>
+					</dl>
+				</div>
+				<div class="element js-animation">
+					<h3>コミュニティへの参加</h3>
+					<h4>領域ごとのコミュニティ形成</h4>
+					<dl>
+						<dt>テーマ例</dt>
+						<dd>・採用/労務/広報/組織活性/育成/制度<br>・メンバー/マネージャー/部門長</dd>
+					</dl>
+				</div>
+				<div class="element js-animation">
+					<h3>イベントの告知・集客</h3>
+					<h4>随時</h4>
+					<dl>
+						<dt>テーマ例</dt>
+						<dd>・人事関連のイベントの告知が可能</dd>
+					</dl>
+				</div>
+				<div class="element js-animation">
+					<h3>セミナーへの参加</h3>
+					<h4>随時実施予定</h4>
+					<dl>
+						<dt>テーマ例</dt>
+						<dd>・人事のキャリアについて<br>・コロナで変わる人事評価の仕方<br>・人事管理領域のシステム・効率化</dd>
+					</dl>
+				</div>
+				<div class="element js-animation">
+					<h3>人事相談箱の利用</h3>
+					<h4>人事に関する質問ができる掲示板</h4>
+					<dl>
+						<dt>例</dt>
+						<dd>・労務について<br>・採用・制度設計のtips<br>・人事キャリアの歩み方</dd>
+					</dl>
+				</div>
+				<!-- box -->
+			</div>
+			<!-- .inner -->
+		</section>
+		<!-- #possible -->		
+
+
+		<!-- #manager -->
+		<section id="manager">
+			<h2 class="element js-animation">運営者情報<br><span>MANAGER</span></h2>
+			<div class="inner">
+				<ul>
+					<li class="element js-animation">
+						<img src="/teaser/img/manager_photo01.png" alt="山中 諭" width="100%">
+						<div>
+							<p>株式会社Hajimari 執行役員</p>
+							<h3>山中 諭</h3>
+							<p>新卒でケンタッキー・フライド・チキンに入社。<br>採用／労務／制度設計／労働組合対応など、人事業務に従事した後、<br>ウィルゲートに入社、人事部を立ち上げ、人事をやりながら<br>広報／経理財務／営業／マーケ／エンジニア部門と幅広くマネジメントを行う。<br>10社以上の人事コンサルに従事し、それと同時に2020年株式会社Hajimariの<br>役員就任。</p>
+						</div>
+					</li>
+					<li class="element js-animation">
+						<div>
+							<p>HRプラス社会保険労務士法人 代表社員</p>
+							<h3>佐藤 広一</h3>
+							<p>「HRに安心、情報、ソリューションをプラスする」をコンセプトに、人事パーソンに<br>コミットした人事労務相談、就業規則見直し、IPO支援、海外労務などの<br>人事労務コンサルティングを展開。複数社で取締役、監査役を兼任。<br>著書30冊を出版、マスコミ・メディア取材、新聞・雑誌への寄稿も積極的に<br>行う傍ら、SMBCコンサルティングなど全国各地で多数の講演を行っている。</p>
+						</div>
+						<img src="/teaser/img/manager_photo02.png" alt="佐藤 広一" width="100%">
+					</li>
+					<li class="element js-animation">
+						<img src="/teaser/img/maneger_photo03.png" alt="小林 伸輔" width="100%">
+						<div>
+							<p>株式会社うるる 取締役</p>
+							<h3>小林 伸輔</h3>
+							<p>新卒で人材系上場会社に入社。トップセールスを獲得した後、<br>2007年に株式会社うるるに参画。<br>当社の中核事業である「入札情報速報サービスNJSS」の立ち上げや事業拡大を<br>担った後、2017年の東証マザーズ上場後は採用・広報管掌を担い組織作りや<br>ブランド作りをミッションとしている。<br>また、うるる社以外での活動として、中高生向けのキャリア支援教育など、<br>若手育成にも力を入れている。</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</section>
+		<!-- #manager -->
+
+
+		<!-- #supporter -->
+		<section id="supporter">
+			<h2 class="element js-animation">サポーター情報<br><span>SUPPORTER</span></h2>
+			<div class="inner">
+				<ul>
+					<li class="element js-animation">
+						<img src="/teaser/img/supporter_photo01.png" alt="清家 良太" width="100%">
+						<p>株式会社<br>GA tecnologies<br>CHRO</p>
+						<h4>清家 良太</h4>
+					</li>
+					<li class="element js-animation">
+						<img src="/teaser/img/supporter_photo02.png" alt="佐藤 雄佑" width="100%">
+						<p>株式会社ミライフ<br>代表取締役</p><br>
+						<h4>佐藤 雄佑</h4>
+					</li>
+					<li class="element js-animation">
+						<img src="/teaser/img/supporter_photo03.png" alt="末永 雄大" width="100%">
+						<p>アクシス株式会社<br>代表取締役</p><br>
+						<h4>末永 雄大</h4>
+					</li>
+					<li class="element js-animation">
+						<img src="/teaser/img/supporter_photo04.png" alt="山口 翔" width="100%">
+						<p>株式会社パンフォーユー<br>取締役</p><br>
+						<h4>山口 翔</h4>
+					</li>
+					<li class="element js-animation">
+						<img src="/teaser/img/supporter_photo05.png" alt="渡辺 兼蔵" width="100%">
+						<p>株式会社トラストリッジ<br>コーポレートデザイン部<br>部長</p>
+						<h4>渡辺 兼蔵</h4>
+					</li>
+				</ul>
+			</div>
+		</section>
+		<!-- #supporter -->
+
+
+		<!-- #entry -->
+		<section id="entry">
+			<h2 class="element js-animation">入会の流れ<br><span>ENTRY</span></h2>
+			<div class="inner">
+				<ul class="element js-animation">
+					<li class="arrow">
+						<h3><img src="/teaser/img/entry_sttl01.png" alt="01" width="100%"></h3>
+						<p>会員登録</p>
+						<img src="/teaser/img/entry_icon01.png" alt="会員登録" width="142">
+					</li>
+					<li class="arrow">
+						<h3><img src="/teaser/img/entry_sttl02.png" alt="02" width="100%"></h3>
+						<p>運営者から<br>コミュニティ招待</p>
+						<img src="/teaser/img/entry_icon02.png" alt="運営者からコミュニティ招待" width="130">
+					</li>
+					<li>
+						<h3><img src="/teaser/img/entry_sttl03.png" alt="03" width="100%"></h3>
+						<p>入会完了</p>
+						<img src="/teaser/img/entry_icon03.png" alt="入会完了" width="151">
+					</li>
+				</ul>
+				<h4 class="element js-animation">入会はこちらから</h4>
+				<a class="element js-animation" href="https://jinjiclub.com/form1">入会はこちら</a>
+			</div>
+		</section>
+		<!-- #entry -->
+
+
+		<!-- #footer -->
+		<footer id="footer">
+			<img src="/teaser/img/logo_square.png" alt="人事倶楽部 JINJICLUB" width="100%">
+		</footer>
+		<!-- #footer -->
+
+	</div>
+	<!-- #contents -->
+
 
 </body>
 </html>
